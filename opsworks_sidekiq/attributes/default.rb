f0031a703bbea3aaa9ee526e5cff7ce9c67a2b87
Vann Gutierrez <vann.g@matadoraz.com>
2015-08-10 13:05:05 -0700

    fixing sidekiq utility cookbook

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -6,10 +6,10 @@ node[:deploy].each do |application, deploy|
 	default[:sidekiq][application.intern] = {}
 	default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
 	default[:sidekiq][application.intern][:syslog] = false
-	override['sidekiq']['dd_rails_api']['production']['process_count'] = 2
-	override['sidekiq']['dd_rails_api']['production']['config']['concurrency'] = 15
-	override['sidekiq']['dd_rails_api']['production']['config']['verbose'] = false
-	override['sidekiq']['dd_rails_api']['production']['config']['queues'] = ['default', 'mailers', 'devise_queue']
-	override['sidekiq']['dd_rails_api']['production']['config']['logfile'] = "./log/sidekiq.log"
+	override['sidekiq']['dd_rails_utility']['production']['process_count'] = 2
+	override['sidekiq']['dd_rails_utility']['production']['config']['concurrency'] = 15
+	override['sidekiq']['dd_rails_utility']['production']['config']['verbose'] = false
+	override['sidekiq']['dd_rails_utility']['production']['config']['queues'] = ['default', 'mailers', 'devise_queue']
+	override['sidekiq']['dd_rails_utility']['production']['config']['logfile'] = "./log/sidekiq.log"
 end
 
