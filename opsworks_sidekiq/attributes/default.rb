0c6ec4907b0fc55103fcef6bea4dae5e3ac8fe1b
Vann Gutierrez <vann.g@matadoraz.com>
2015-09-03 09:06:56 -0700

    working on sidekiq cookbook upgrades

--- opsworks_sidekiq/attributes/default.rb
+++ opsworks_sidekiq/attributes/default.rb
@@ -3,13 +3,13 @@ include_attribute 'deploy'
 default[:sidekiq] = {}
 
 node[:deploy].each do |application, deploy|
-	default[:sidekiq][application.intern] = {}
-	default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
-	default[:sidekiq][application.intern][:syslog] = false
-	override['sidekiq']['dd_rails_utility']['production']['process_count'] = 2
-	override['sidekiq']['dd_rails_utility']['production']['config']['concurrency'] = 15
-	override['sidekiq']['dd_rails_utility']['production']['config']['verbose'] = false
-	override['sidekiq']['dd_rails_utility']['production']['config']['queues'] = ['default', 'mailers', 'devise_queue']
-	override['sidekiq']['dd_rails_utility']['production']['config']['logfile'] = "./log/sidekiq.log"
+  default[:sidekiq][application.intern] = {}
+  default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
+  default[:sidekiq][application.intern][:syslog] = false
+  override['sidekiq']['drop_dental_api']['staging']['process_count'] = 4
+  override['sidekiq']['drop_dental_api']['staging']['config']['concurrency'] = 20
+  override['sidekiq']['drop_dental_api']['staging']['config']['verbose'] = false
+  override['sidekiq']['drop_dental_api']['staging']['config']['queues'] = ['default', 'mailers', 'devise_queue']
+  override['sidekiq']['drop_dental_api']['staging']['config']['logfile'] = "./log/sidekiq.log"
 end
 
